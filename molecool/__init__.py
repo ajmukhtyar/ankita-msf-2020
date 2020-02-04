3e83b526dff3fab6e6d7574398c5d86cc64e4a41
Ankita Mukhtyar <ankitamukhtyar@Ankitas-MacBook-Pro.local>
2020-02-04 10:35:55 -0500

    group functions into modules and subpackages

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -6,6 +6,11 @@ A python package for the MSF 2020A Bootcamp
 # Add imports here
 from .functions import *
 from .atomdata import atomic_weights, atom_colors
+from .measure import calculate_distance,calculate_angle
+from .molecule import build_bond_list
+from .visualize import bond_histogram, draw_molecule
+
+import molecool.molecool_io
 
 # Handle versioneer
 from ._version import get_versions
---
+++ molecool/molecool_io/__init__.py
@@ -0,0 +1,6 @@
+"""
+molecool
+A python package for the MSF 2020A Bootcamp
+"""
+from .pdb import open_pdb
+from .xyz import open_xyz, write_xyz 
